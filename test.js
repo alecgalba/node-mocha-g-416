6ce871b4ef14d1bcd58834b7c11c1f9eb97dd093
alecgalba <alecgalba@gmail.com>
2018-01-31 10:16:58 -0500

    tests pass

--- test.js
+++ test.js
@@ -1,11 +1,11 @@
 
 var expect = require('chai').expect
-var name = 'React Quickly'
+var name = 'React Slowly'
 var url = ['http://reactquickly.co', 'https://www.manning.com/books/react-quickly']
 describe('name and url', function() {
   it('must match the values', function(done){
     expect(name).to.be.a('string')
-    expect(name).to.equal('React Quickly')
+    expect(name).to.equal('React Slowly')
     expect(url).to.have.length(2)
     expect(url).to.have.deep.property('[1]', 'https://www.manning.com/books/react-quickly')
       .with.length(43)
